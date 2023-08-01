9e233fe6ce3160aad02f81557a48471e4316b0b2
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2023-08-01 23:19:13 +0530

    server init

---
+++ todo-java-api/src/main/java/com/schndra/todojavaapi/TodoJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.schndra.todojavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoJavaApiApplication.class, args);
+	}
+
+}
---
+++ todo-java-api/src/test/java/com/schndra/todojavaapi/TodoJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schndra.todojavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
