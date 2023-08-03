65971094c559a6850c3950f3a8b7e9e32d949a36
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2023-08-03 22:21:49 +0530

    added @AllArgsConstructor & @NoArgsContructor

--- todo-java-api/src/main/java/com/schndra/todojavaapi/model/Todo.java
+++ todo-java-api/src/main/java/com/schndra/todojavaapi/model/Todo.java
@@ -3,9 +3,13 @@ package com.schndra.todojavaapi.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data // automatically add getters and setters
+@AllArgsConstructor //constructor with all the arguments
+@NoArgsConstructor // constructor with no arguments
 public class Todo {
     private long id;
     private String title;
