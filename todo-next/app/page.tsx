398c2530e8014595e9c6e73d3755a5c07aad2907
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2023-08-10 22:47:29 +0530

    fetch data on server

--- todo-next/app/page.tsx
+++ todo-next/app/page.tsx
@@ -1,7 +1,8 @@
+import { getAllTodos } from "@/utils/todo-service";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 
-export default function Home() {
+  const todos = await getAllTodos();
   return (
     <main className="grid h-screen">
       <section className="p-4 place-self-center rounded-lg bg-slate-200 w-[80vw] sm:w-[450px] ">
