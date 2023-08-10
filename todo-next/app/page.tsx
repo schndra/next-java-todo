519f96a486a71ec7779042cc4468a3dd2eae91c2
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2023-08-10 22:50:42 +0530

    pass down fetched data into todoList component

--- todo-next/app/page.tsx
+++ todo-next/app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
       <section className="p-4 place-self-center rounded-lg bg-slate-200 w-[80vw] sm:w-[450px] ">
         <h1 className="text-center text-2xl font-bold">My Todo App</h1>
         <AddTodo />
-        <TodoList />
+        <TodoList todos={todos} />
       </section>
     </main>
   );
