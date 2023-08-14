7589e537281fb61b227134b78d0c152697adb968
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2023-08-14 23:39:11 +0530

    add form to submit todo

--- todo-next/app/components/AddTodo.tsx
+++ todo-next/app/components/AddTodo.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { createTodo } from "@/utils/todo-service";
 import { FormEventHandler, useState } from "react";
 import { FaPlus } from "react-icons/fa6";
 
 const AddTodo = () => {
+  const [title, setTitle] = useState<string>("");
 
   const handleSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
@@ -17,16 +19,29 @@ const AddTodo = () => {
   };
 
   return (
-    <div className="flex justify-center items-center gap-2 mt-4 flex-col">
-      <input type="text" className="p-2 w-full rounded-md" />
-      <button className="flex items-center justify-center gap-1 p-2 w-full bg-slate-950 rounded-md text-slate-100 uppercase hover:bg-slate-900">
-        <FaPlus />
-        <FaPlus size={10} />
-        Add Todo
-        <FaPlus size={10} />
-        <FaPlus />
-      </button>
-    </div>
+    <>
+      <form
+        className="flex justify-center items-center gap-2 mt-4 flex-col"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          className="p-2 w-full rounded-md"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button
+          className="flex items-center justify-center gap-1 p-2 w-full bg-slate-950 rounded-md text-slate-100 uppercase hover:bg-slate-900"
+          type="submit"
+        >
+          <FaPlus />
+          <FaPlus size={10} />
+          Add Todo
+          <FaPlus size={10} />
+          <FaPlus />
+        </button>
+      </form>
+    </>
   );
 };
 export default AddTodo;
