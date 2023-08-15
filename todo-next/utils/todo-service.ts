45309a17ae38bbb467101968dee7fdba1369ff7f
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2023-08-15 22:11:09 +0530

    Add dynamic data fetching

--- todo-next/utils/todo-service.ts
+++ todo-next/utils/todo-service.ts
@@ -3,7 +3,7 @@ import { ITodo } from "../types/todos";
 const baseUrl = "http://localhost:8080";
 
 export const getAllTodos = async (): Promise<ITodo[]> => {
-  const res = await fetch(`${baseUrl}/api/v1/todos`);
+  const res = await fetch(`${baseUrl}/api/v1/todos`, { cache: "no-store" });
   const todos = await res.json();
   return todos;
 };
