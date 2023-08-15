a5c571e258afe6214d273d1b2629d9a439869b96
Abhishek Sachindra <73639997+schndra@users.noreply.github.com>
2023-08-15 22:40:35 +0530

    complete updateTodo

--- todo-next/app/components/TodoItem.tsx
+++ todo-next/app/components/TodoItem.tsx
@@ -1,22 +1,38 @@
+"use client";
+
+import { useState } from "react";
 import { ITodo } from "@/types/todos";
 import { FaTrash } from "react-icons/fa6";
 import { FaSquareCheck, FaRegSquareCheck } from "react-icons/fa6";
+import { updateTodo } from "@/utils/todo-service";
+import { useRouter } from "next/navigation";
 
 interface TodoItemProps {
   todo: ITodo;
 }
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+  const router = useRouter();
+
+  const handleClick = async (todo: ITodo) => {
+    await updateTodo(todo);
+    router.refresh();
+  };
+
   return (
     <li className="p-2 flex justify-between items-center ">
       <div className="flex gap-2 items-center">
         {todo.completed ? (
-          <button>
-            <FaRegSquareCheck />
+          <button
+            onClick={() => handleClick({ ...todo, completed: !todo.completed })}
+          >
+            <FaSquareCheck />
           </button>
         ) : (
-          <button>
-            <FaSquareCheck />
+          <button
+            onClick={() => handleClick({ ...todo, completed: !todo.completed })}
+          >
+            <FaRegSquareCheck />
           </button>
         )}
         <p>{todo.title}</p>
